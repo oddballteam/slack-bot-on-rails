c2b7efa08af7815c5d24a7eb98fa629728d9969f
Michael R. Fleet <f1337@f1337.us>
2020-09-04 11:45:34 -0400

    add 'say' slack bot

--- config.ru
+++ config.ru
@@ -1,5 +1,11 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
+require_relative 'lib/bot/say'
+
+Thread.abort_on_exception = true
+Thread.new do
+  Say.run
+end
 
 run Rails.application
---
+++ lib/bot/say.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# a simple slack echo bot
+class Say < SlackRubyBot::Bot
+  @id = 0
+
+  def self.next_id
+    @id = @id % 10 + 1
+  end
+
+  command 'say' do |client, data, match|
+    Rails.cache.write next_id, { text: match['expression'] }
+    client.say(channel: data.channel, text: match['expression'])
+  end
+end
