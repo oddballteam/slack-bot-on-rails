883d9c04d433004e92154f7364a19f3aa6e7b27a
Michael R. Fleet <f1337@f1337.us>
2020-09-09 13:44:34 -0400

    Revert "remove activerecord"
    This reverts commit 15437386e737934e174460a6379587057edda073.

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,5 @@
 require_relative 'boot'
-
-require 'rails'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'active_model/railtie'
-require 'action_view/railtie'
-require 'sprockets/railtie'
-require 'rails/test_unit/railtie'
-require 'action_cable/engine'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
