e81eae8182121b48fe36a1eddec740da55f19581
Michael R. Fleet <f1337@f1337.us>
2020-09-26 12:15:06 -0400

    disable legacy slack api token

--- config/initializers/slack_client.rb
+++ config/initializers/slack_client.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 # legacy API token config:
-Slack.configure do |config|
-  config.token = ENV['SLACK_API_TOKEN']
-end
+# Slack.configure do |config|
+#   config.token = ENV['SLACK_API_TOKEN']
+# end
