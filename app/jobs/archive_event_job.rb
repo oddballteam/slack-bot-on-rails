efb065e48b80b251fcb6406ddfd1c2fa3f79b3d6
Michael R. Fleet <f1337@f1337.us>
2020-09-27 00:13:48 -0400

    classnames for zeitwerk crashy

--- app/jobs/archive_event_job.rb
+++ app/jobs/archive_event_job.rb
@@ -2,3 +2,5 @@
 # log to Loki for long-term storage
 # delete the event (or status: archived?)
 # is Loki or Postgres best for this data to be warehoused?
+class ArchiveEventJob < ApplicationJob
+end
--- app/jobs/create_issue_job.rb
+++ app/jobs/create_issue_job.rb
@@ -3,3 +3,5 @@
 # - state: ticketed
 # - post update to Slack with link to github ticket
 #   - hype how this process helps us to solve 100% of issues via documentation?
+class CreateIssueJob < ApplicationJob
+end
